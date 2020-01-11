1efc42f1dc15b4b3c9c81a0c4fd385bdb21e730a
szkjtv <szkjtv@outlook.com>
2020-01-11 19:55:56 +0800

    更新说明

--- add.go
+++ add.go
@@ -1,4 +1,4 @@
-package main
+package test
 
 func add(x, y int) int {
 
---
+++ testpkg/add.go
@@ -0,0 +1,12 @@
+package test
+
+func add(x, y int) int {
+
+	return x + y
+
+}
+
+// func main() {
+// 	prin := add(10, 55)
+// 	fmt.Println(prin)
+// }
